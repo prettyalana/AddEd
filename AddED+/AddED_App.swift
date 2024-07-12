58a77715b76560fec461a204b7bb8167ff479679
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-07-12 01:39:00 -0500

    Initial Commit

---
+++ AddED+/AddED_App.swift
@@ -0,0 +1,17 @@
+//
+//  AddED_App.swift
+//  AddED+
+//
+//  Created by Alana Edwards on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AddED_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
