7e59dc744ad74473cd63d3d30927553be1b54ce6
3 GO Gantz <3gogantz@3-MacBook-Air-of-Linda--Bill-Gantz-BGCC.local>
2024-07-18 16:27:57 -0500

    I added the view for the review stars that will change their view based on the rating. I also started the program view but I haven't done the reviews section yet.

---
+++ AddED+/Views/ProgramView.swift
@@ -0,0 +1,93 @@
+//
+//  ProgramView.swift
+//  AddED+
+//
+//  Created by 3 GO Gantz on 7/18/24.
+//
+
+import SwiftUI
+
+struct ProgramView: View {
+    
+    @State var displayedProgram: Program
+    
+    var body: some View {
+        
+        VStack{
+            //Top green bar
+            ZStack {
+                Rectangle()
+                    .fill(.green)
+                    .frame(width:UIScreen.main.bounds.width, height:90)
+                //insert logo here
+            }
+            ScrollView {
+                HStack{
+                    Image("backArrow")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                    
+                    Spacer()
+                    
+                    Text(displayedProgram.name)
+                        .font(.largeTitle)
+                        .padding()
+                    Spacer()
+                    
+                    
+                    
+                }
+                
+                VStack {
+                    HStack {
+                        Image("Placeholder")
+                            .resizable()
+                            .frame(width: 300, height: 200)
+                        
+                        
+                        
+                    }
+                    
+                    RatingStarsView(programRating: ECCChicago, test: 3.7)
+                }
+                
+                HStack {
+                    TagRectangleView(displayTag: technologyTag, removable: false, itemTagged: ECCChicago)
+                    
+                    
+                    TagRectangleView(displayTag: paidTag, removable: false, itemTagged: ECCChicago)
+                }
+                .padding(.top)
+                
+                GroupBox(label: Label("Description" , systemImage: "")) {
+                    Text(displayedProgram.description)
+                }
+                .frame(width: 300, height: 200)
+                
+                Text("Reviews")
+                    .font(.title)
+                ZStack{
+                    HStack {
+                        Spacer()
+                        Color.gray
+                        Spacer()
+                    }
+                    
+                    
+                    
+                    
+                }
+                
+                
+                
+                
+                
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    ProgramView(displayedProgram: ECCChicago)
+}
---
+++ AddED+/Views/RatingStarsView.swift
@@ -0,0 +1,78 @@
+//
+//  RatingStarsView.swift
+//  AddED+
+//
+//  Created by 3 GO Gantz on 7/18/24.
+//
+
+import SwiftUI
+
+struct RatingStarsView: View {
+    
+    @State var programRating: Program
+    var test: Double
+    
+    var body: some View {
+        
+        
+        HStack {
+            
+            
+            if test >= 4.5 {
+                Image(systemName:"star.fill")
+                Image(systemName:"star.fill")
+                Image(systemName:"star.fill")
+                Image(systemName:"star.fill")
+                Image(systemName:"star.fill")
+            }
+            else if test >= 3.5 {
+                Image(systemName:"star.fill")
+                Image(systemName:"star.fill")
+                Image(systemName:"star.fill")
+                Image(systemName:"star.fill")
+                Image(systemName:"star")
+                
+            }
+            else if test >= 2.5 {
+                Image(systemName:"star.fill")
+                Image(systemName:"star.fill")
+                Image(systemName:"star.fill")
+                Image(systemName:"star")
+                Image(systemName:"star")
+            }
+            else if test >= 1.5 {
+                Image(systemName:"star.fill")
+                Image(systemName:"star.fill")
+                Image(systemName:"star")
+                Image(systemName:"star")
+                Image(systemName:"star")
+            }
+            else if test >= 0.5 {
+                Image(systemName:"star.fill")
+                Image(systemName:"star")
+                Image(systemName:"star")
+                Image(systemName:"star")
+                Image(systemName:"star")
+            }
+            else {
+                Image(systemName:"star")
+                Image(systemName:"star")
+                Image(systemName:"star")
+                Image(systemName:"star")
+                Image(systemName:"star")
+                
+            }
+            
+            
+//            Image(systemName:"star")
+//            Image(systemName:"star.fill")
+                
+        }
+        
+    }
+    
+}
+
+#Preview {
+    RatingStarsView(programRating: ECCChicago, test: 3.7)
+}
