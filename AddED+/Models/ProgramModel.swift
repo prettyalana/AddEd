25e5d3e89a6672e47018027d6ce04a776074f7fa
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-07-22 14:56:42 -0500

    Removed comments in the ProgramModel

--- AddED+/Models/ProgramModel.swift
+++ AddED+/Models/ProgramModel.swift
@@ -114,12 +114,4 @@ var cyberSecurityProgram = Program(inputName: "Intro to Cybersecurity", inputDes
 var placeholderPrograms: [Program] = [ECCChicago, afterSchoolMatters, cyberSecurityProgram, webDevProgram]
 var placeholderFeaturedPrograms: [Program] = [ECCChicago, afterSchoolMatters]
 
-
-//var sampleData:  [Program] = [
-//    Program(inputName: "Intro to Cybersecurity", inputDescription: "something", inputTime: "07/25/24",inputDays: "08/25/24", inputPlace: "Chicago, IL"),
-//    Program(inputName: "Intro to Web Development", inputDescription: "something", inputTime: "07/25/24",inputDays: "08/25/24", inputPlace: "Chicago, IL"),
-//        Program(id: 1, name: "Everyone Can Code", description: "The Everyone Can Code Chicago initiative was launched as a public-private partnership that expands opportunities for youth to develop coding skills and explore career pathways.  Through this initiative, employees at local businesses share their professional experience with youth.  And the youth have the opportunity to gain work experience through internships.", startDate: "06/24/24", endDate: "08/05/24", place: "Chicago, IL"),
-//        Program(id: 2, name: "After School Matters", description: "After School Matters inspires Chicago’s teens to discover their passions, develop skills for life beyond high school, and make friends along the way.", startDate: "07/16/24", endDate: "07/16/25", place: "Chicago, IL"),
-    ]
-
 var currentUser = User(name: "John Doe", username: "johndoe", interests: ["Technology"], email: "johndoe@example.com", password: "example", savedPrograms: [ECCChicago])
