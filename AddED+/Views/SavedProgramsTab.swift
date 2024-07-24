8d6575a6981fa4c0312bb1816cfe950346fb233c
2 GO Gantz <2gogantz@2-MacBook-Air-of-Linda--Bill-Gantz-BGCC.local>
2024-07-24 14:22:17 -0500

    Created SavedProgramsTab View

---
+++ AddED+/Views/SavedProgramsTab.swift
@@ -0,0 +1,39 @@
+//
+//  SavedProgramsTab.swift
+//  AddED+
+//
+//  Created by 2 GO Gantz on 7/22/24.
+//
+
+import SwiftUI
+
+struct SavedProgramsTab: View {
+    var body: some View {
+        VStack{
+            HStack {
+                GreenHeader()
+            }
+            HStack {
+                Text("Saved")
+                    .font(.title)
+                    .fontWeight(.medium)
+                    .foregroundColor(.black)
+                    .frame(alignment: .top)
+            }
+            ScrollView {
+                VStack {
+                    ForEach(placeholderFeaturedPrograms) {
+                        featuredProgram in
+                        ProgramCardView(displayedProgram: featuredProgram, areTagsRemovable: false, paddingAmount: 20.0)
+                    }
+                }
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+
+#Preview {
+    SavedProgramsTab()
+}
