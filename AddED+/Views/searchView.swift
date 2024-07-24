3aff56f3e2ec85b4a29c8ecaca226f1c237e50db
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-07-24 00:19:43 -0500

    Created searchView

---
+++ AddED+/Views/searchView.swift
@@ -0,0 +1,18 @@
+//
+//  searchView.swift
+//  AddED+
+//
+//  Created by Alana Edwards on 7/24/24.
+//
+
+import SwiftUI
+
+struct searchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    searchView()
+}
