99fd7b9dfb713333c49f21fe24bbaf7ed4b27f75
6 GO Gantz <6gogantz@6-MacBook-Air-of-Linda--Bill-Gantz-BGCC.local>
2024-07-24 16:16:37 -0500

    Started work on search page and changed program view background

---
+++ AddED+/Views/TagListView.swift
@@ -0,0 +1,19 @@
+//
+//  TagListView.swift
+//  AddED+
+//
+//  Created by 6 GO Gantz on 7/24/24.
+//
+
+import SwiftUI
+
+struct TagListView: View {
+    @State var usedTag: Tag
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TagListView(usedTag: technologyTag)
+}
